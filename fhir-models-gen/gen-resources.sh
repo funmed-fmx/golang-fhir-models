ccdbe05cda1e6d326e4eee17c2f2ad7d66d2a0c2
Mikael Karlsson <mmmicke@gmail.com>
2022-08-23 07:54:57 +0200

    Fix incorrect path in gen script

--- fhir-models-gen/gen-resources.sh
+++ fhir-models-gen/gen-resources.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 wget -O definitions.zip https://www.hl7.org/fhir/definitions.json.zip
-unzip definitions.zip definitions.json/profiles-types.json definitions.json/valuesets.json -d ./fhir
+unzip -o definitions.zip definitions.json/profiles-types.json definitions.json/valuesets.json
 mv definitions.json/profiles-types.json fhir/
 mv definitions.json/valuesets.json fhir/
 rm -rf definitions.zip definitions.json
-rm definitions.zip
+
 wget -O fhir/bundle.json http://hl7.org/fhir/bundle.profile.json
 wget -O fhir/codesystem.json http://hl7.org/fhir/codesystem.profile.json
 wget -O fhir/structuredefinition.json http://hl7.org/fhir/structuredefinition.profile.json
